13efa5e7f30a41375d47202bf3ca6356ce644722
Senten <wuitenye@163.com>
2022-07-12 14:36:40 +0800

    可以生成文件存储时间及步数信息

--- wechatSport.js
+++ wechatSport.js
@@ -1,22 +1,17 @@
 auto()
-toast(' 11111')
+
 click(360,1194)
 sleep(3000);
-toast("success into ranking")
 
 const today = new Date(Date.now());
 const date1 = today.toISOString().split('T')[0]; // "2020-06-13T18:30:00.000Z"
 var timenow = today.toISOString().split('T')[1].slice(0,12)
-toast(date1,timenow)
+
 var path = "/sdcard/"+date1+'.txt'
-id("c5a").untilFind().forEach(child => {
-    var target = child.text();
+sportTable = id("c5a").untilFind().toArray();
+for( var i = 0; typeof(sportTable[i]) != 'undefined'; i++){
+    var target = sportTable[i].text();
     var wtext = target + '   '+ timenow + '\n'
-    toast(wtext)
-    // Files.append(path,wtext)
-    });
-// id("c68").findOne().children().forEach(child => {
-//     var text = child.findOne(id("c5a")).text();
-//     toast(text);
-//     sleep(1000)
-//     });
+    log(wtext)
+    files.append(path,wtext)
+    }
