5572ac522be87ed2aed2d4f86acbce9de38b4ce2
Senten <wuitenye@163.com>
2022-07-12 15:45:41 +0800

    基本功能已实现

--- wechatSport.js
+++ wechatSport.js
@@ -1,19 +1,31 @@
 auto()
+text('微信运动').findOnce().parent().parent().parent().parent().click()
 
-click(360,1194)
-sleep(3000);
+function captureSportsData(){
+    text('步数排行榜').waitFor()
+    text('步数排行榜').findOnce().parent().parent().click()
 
-const today = new Date(Date.now());
-const date1 = today.toISOString().split('T')[0]; // "2020-06-13T18:30:00.000Z"
-var timenow = today.toISOString().split('T')[1].slice(0,12)
+    //not recommand the following method (Don't universal)
+    // click(360,1194)
+    sleep(2000);
 
-var path = "/sdcard/"+date1+'.txt'
-stepTable = id("c5a").untilFind().toArray();
-userTable = id("c6d").untilFind().toArray();
-for( var i = 0; typeof(stepTable[i]) != 'undefined'; i++){
-    var targetStep = stepTable[i].text();
-    var targetUser = userTable[i].text();
-    var wtext = targetUser + '-' + targetStep + '-'+ timenow + '\n'
-    log(wtext)
-    files.append(path,wtext)
+    const today = new Date(Date.now());
+    const date1 = today.toISOString().split('T')[0]; // "2020-06-13T18:30:00.000Z"
+    var timenow = today.toISOString().split('T')[1].slice(0,12)
+
+    var path = "/sdcard/"+date1+'.txt'
+    stepTable = id("c5a").untilFind().toArray();
+    userTable = id("c6d").untilFind().toArray();
+    for( var i = 0; typeof(stepTable[i]) != 'undefined'; i++){
+        var targetStep = stepTable[i].text();
+        var targetUser = userTable[i].text();
+        var wtext = targetUser + '-' + targetStep + '-'+ timenow + '\n'
+        log(wtext)
+        files.append(path,wtext)
+    }
+    id('ei').findOne().parent().click()
+}
+while (true){
+    captureSportsData();
+    sleep(60*1000);//sleep for 30s, You should sleep at least for 1 min in real to avoid ip refrain. 
 }
