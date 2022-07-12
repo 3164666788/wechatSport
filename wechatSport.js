eefea23c40374c99ea16eed4eb47843be744a4db
Senten <wuitenye@163.com>
2022-07-12 14:50:57 +0800

    加入用户名搜寻

--- wechatSport.js
+++ wechatSport.js
@@ -8,10 +8,12 @@ const date1 = today.toISOString().split('T')[0]; // "2020-06-13T18:30:00.000Z"
 var timenow = today.toISOString().split('T')[1].slice(0,12)
 
 var path = "/sdcard/"+date1+'.txt'
-sportTable = id("c5a").untilFind().toArray();
-for( var i = 0; typeof(sportTable[i]) != 'undefined'; i++){
-    var target = sportTable[i].text();
-    var wtext = target + '   '+ timenow + '\n'
+stepTable = id("c5a").untilFind().toArray();
+userTable = id("c6d").untilFind().toArray();
+for( var i = 0; typeof(stepTable[i]) != 'undefined'; i++){
+    var targetStep = stepTable[i].text();
+    var targetUser = userTable[i].text();
+    var wtext = targetUser + '-' + targetStep + '-'+ timenow + '\n'
     log(wtext)
     files.append(path,wtext)
-    }
+}
